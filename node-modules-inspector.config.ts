d550a6896a02f9bd2a62c92db83c8d165e548f17
Anthony Fu <github@antfu.me>
2025-02-18 22:37:37 +0800

    feat: deploy

--- node-modules-inspector.config.ts
+++ node-modules-inspector.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'node-modules-inspector'
 
 export default defineConfig({
+  name: '@antfu/everything',
+  defaultFilters: {
+    'source-type': 'prod',
+    'exclude-workspace': true,
+  },
   excludeDependenciesOf: [
     'eslint',
     'webpack',
+  ],
+  excludePackages: [
+    'typescript',
   ],
 })
